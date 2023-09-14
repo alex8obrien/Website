5a9d11a47cd10faa34cf8b182c663cf4d9972605
Alex O'Brien <alex8obrien@gmail.com>
2023-09-14 20:48:45 +0100

    updated website metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://www.alex8obrien.com',
 	integrations: [mdx(), sitemap()],
 });
