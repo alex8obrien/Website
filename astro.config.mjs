8493304b043f8fd54e3368c330ba71bf86ddcda8
Alex O'Brien <alex8obrien@gmail.com>
2023-09-20 00:07:01 +0100

    FIX: SSR with blogs rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
