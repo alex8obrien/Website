5eb63c0da710fbd31067f6f8fecfa8878b787923
Alex O'Brien <alex8obrien@gmail.com>
2023-09-26 16:10:55 +0100

    Added VitePWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from "@astrojs/cloudflare";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.alex8obrien.com',
   integrations: [mdx(), sitemap()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  plugins: [
+    VitePWA({ registerType: 'autoUpdate' })
+  ]
 });
